00a7a64f8d5e30c71b9c1723dd0e2a4bf9737b7e
Zpub <lil.rhymes9@gmail.com>
2024-01-09 02:09:10 +0300

    Навигация по страницам при помощи react router dom

---
+++ src/Main.js
@@ -0,0 +1,5 @@
+export function Main(params) {
+    return (
+        <div>Main</div>
+    )
+}
---
+++ src/Profile.js
@@ -0,0 +1,5 @@
+export function Profile(params) {
+    return (
+        <div>Profile</div>
+    )
+}
