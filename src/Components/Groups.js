09f5228bf57d4838e91c715dad6180ef42041695
Zpub <lil.rhymes9@gmail.com>
2024-01-14 19:41:53 +0300

    Выделение названия группы

--- src/Components/Groups.js
+++ src/Components/Groups.js
@@ -1,8 +1,9 @@
-import { Box, Container, Fade, Popper } from "@mui/material";
+import { Box, Container, Fade, Paper, Popper } from "@mui/material";
 import { Nav } from "../Utils/Nav";
 import { SideBar } from "../Utils/SideBar";
 import standartavatar from "../Images/standartavatar.jpg";
 import { GroupListMenu } from "../Utils/GroupListMenu";
+import '../CSS/Groups.css';
 
 
 export function Groups(params) {
@@ -12,23 +13,41 @@ export function Groups(params) {
 
             <Container style={{ display: "flex", marginTop: "20px" }}>
                 <SideBar></SideBar>
-                <div className="cards" style={{ width: "100%", height: "100%" }}>
-                    <div className="card" style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-                        <div className="left" style={{ display: "flex" }}>
-                            <div className="image">
-                                <img width={"64px"} height={"64px"} src={standartavatar} style={{ borderRadius: "200px", marginRight: "10px" }}></img>
+
+                <Paper style={{width:"100%", padding:"8px"}}>
+                    <div className="cards" style={{ width: "100%", height: "100%" }}>
+                        <div className="card" style={{ display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+                            <div className="left" style={{ display: "flex" }}>
+                                <div className="image">
+                                    <img width={"64px"} height={"64px"} src={standartavatar} style={{ borderRadius: "200px", marginRight: "10px" }}></img>
+                                </div>
+                                <div>
+                                    <h3>Группа</h3>
+                                    <p>Категория</p>
+                                    <p>Подписчики</p>
+                                </div>
                             </div>
-                            <div>
-                                <p>Группа</p>
-                                <p>Категория</p>
-                                <p>Подписчики</p>
+                            <div className="right" style={{}}>
+                                <GroupListMenu></GroupListMenu>
                             </div>
                         </div>
-                        <div className="right" style={{}}>
-                            <GroupListMenu></GroupListMenu>
-                        </div>
+                        <div className="card" style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                            <div className="left" style={{ display: "flex" }}>
+                                <div className="image">
+                                    <img width={"64px"} height={"64px"} src={standartavatar} style={{ borderRadius: "200px", marginRight: "10px" }}></img>
+                                </div>
+                                <div>
+                                    <p>Группа</p>
+                                    <p>Категория</p>
+                                    <p>Подписчики</p>
+                                </div>
+                            </div>
+                            <div className="right" style={{}}>
+                                <GroupListMenu></GroupListMenu>
+                            </div>
+                        </div>  
                     </div>
-                </div>
+                </Paper>
             </Container>
         </div>
     )
--- src/Utils/GroupListMenu.js
+++ src/Utils/GroupListMenu.js
@@ -6,17 +6,15 @@ import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
 import menu from "../Images/menu.png";
 
 
-export function GroupListMenu(params) {
+export function GroupListMenu(props) {
     return (
         <PopupState variant="popover" popupId="demo-popup-menu">
             {(popupState) => (
                 <React.Fragment>
-                    <Button variant="text" {...bindTrigger(popupState)}>
-                        <img className="menu_image" src={menu} width={"32px"} height={"32px"} />
-                    </Button>
+                    <img className="menu_image" src={menu} style={{cursor:"pointer"}} width={"32px"} height={"32px"} {...bindTrigger(popupState)} />
                     <Menu {...bindMenu(popupState)} >
-                        <div style={{ width: "200px", height:"100px" }}>
-                            <MenuItem onClick={popupState.close}>Profile</MenuItem>
+                        <div style={{ width: "200px", height: "100px" }}>
+                            <MenuItem onClick={popupState.close}>Отписаться</MenuItem>
                             <MenuItem onClick={popupState.close}>My account</MenuItem>
                             <MenuItem onClick={popupState.close}>Logout</MenuItem>
                         </div>
